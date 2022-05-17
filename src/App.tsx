521610dde72554f42d6b2670ba075b191732cdf4
EugeneMorash <85665094+EugeneMorash@users.noreply.github.com>
2022-05-17 18:19:09 +0300

    todolist2 map done

--- src/App.tsx
+++ src/App.tsx
@@ -11,17 +11,17 @@ export type ScheduleType = {
 
 const schedule = [
     {id: 101, item: "Waking up", isDone: true},
-    {id: 102, item: "Exercise", isDone: true},
-    {id: 103, item: "Washing", isDone: true},
+    {id: 102, item: "Exercise", isDone: false},
+    {id: 103, item: "Washing", isDone: false},
     {id: 104, item: "Breakfast", isDone: true},
     {id: 105, item: "Lazy time", isDone: true},
     {id: 106, item: "Lunch", isDone: true},
-    {id: 107, item: "Horse racing", isDone: true},
+    {id: 107, item: "Horse racing", isDone: false},
     {id: 108, item: "Afternoon tea", isDone: true},
     {id: 109, item: "Snooker", isDone: true},
     {id: 110, item: "Dinner", isDone: true},
     {id: 111, item: "Cigar time", isDone: true},
-    {id: 112, item: "Washing", isDone: true},
+    {id: 112, item: "Washing", isDone: false},
     {id: 113, item: "Putting to bed", isDone: true}
 ]
 
--- src/components/Todolist2.tsx
+++ src/components/Todolist2.tsx
@@ -8,12 +8,19 @@ export type Todolist2PropsType = {
 }
 
 export function Todolist2(props: Todolist2PropsType) {
+
+    const itemArr = props.schedule.map(item => {
+        return <li key={item.id}>
+            <input type='checkbox' checked={item.isDone} />
+               <span>{item.item}</span>
+        </li>
+    })
     return (
-
-
        <div>
            <h3>props.heading</h3>
-           {props.schedule}
+           <div>
+           {itemArr}
+           </div>
        </div>
     )
 };
